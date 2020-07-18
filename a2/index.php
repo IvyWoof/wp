00f4f60b19ed9669cb5b6827556e6e9d12e0b30f
Iveosk <55953354+Iveosk@users.noreply.github.com>
2020-07-18 16:15:28 +0100

    Changed page structure, added 3 relevant images and moved header next to navbar

--- a2/index.php
+++ a2/index.php
@@ -12,31 +12,42 @@
   <body>
 
     <header>
-      		<h1 class="">ANZAC Douglas Raymond Baker</h1>
-      		<img src="../../media/D.R.Baker.jpg" alt="D.R. Baker F.Co">
+        <h1>ANZAC Douglas Raymond Baker</h1>
+        <!--<img src="../../media/D.R.Baker.jpg" alt="D.R. Baker F.Co">-->
+      <nav>
+    		<ul class="navigation">
+  		    <li><a href="#">Home</a></li>
+  		   	<li><a href="#">Introduction</a></li>
+  		   	<li><a href="#">Letters & Post Cards</a></li>
+  		   		<ul>
+  		   			<li><a href="#">1914</a></li>
+  	    			<li><a href="#">1915</a></li>
+  	    			<li><a href="#">1916</a></li>
+  		    		<li><a href="#">1917</a></li>
+  		    		<li><a href="#">1918</a></li>
+  		    	</ul>
+  		   	<li><a href="#">Descriptions of Battle Action</a></li>
+  		   	<li><a href="#">Places</a></li>
+  		   	<li><a href="#">Related Material</a></li>
+    		</ul>
+    	</nav>
     </header>
 
-    <nav>
-  		<ul id="navigation">
-		    <li><a href="#">Home</a></li>
-		   	<li><a href="#">Introduction</a></li>
-		   	<li><a href="#">Letters & Post Cards</a></li>
-		   		<ul>
-		   			<li><a href="#">1914</a></li>
-	    			<li><a href="#">1915</a></li>
-	    			<li><a href="#">1916</a></li>
-		    		<li><a href="#">1917</a></li>
-		    		<li><a href="#">1918</a></li>
-		    	</ul>
-		   	<li><a href="#">Descriptions of Battle Action</a></li>
-		   	<li><a href="#">Places</a></li>
-		   	<li><a href="#">Related Material</a></li>
-  		</ul>
-  	</nav>
-
     <main>
-      <article id='Website Under Construction'>
-      </article>
+      <section class="selectContainer">
+        <img src="../../media/letters-home.jpg" class="selectImage"  alt="Soldier writing letter">
+        <div class="selectOverlay">
+          <div class="selectText">Letters and Postcards</div>
+        </div>
+        <img src="../../media/battles.png" class="selectImage"  alt="World War 2 Aircraft">
+        <div class="selectOverlay">
+          <div class="selectText">Battles</div>
+        </div>
+        <img src="../../media/maps.png" class="selectImage"  alt="Map of World War 2 countries, allies and enemies">
+        <div class="selectOverlay">
+          <div class="selectText">Locations and Places</div>
+        </div>
+      </section>
     </main>
 
     <footer>
