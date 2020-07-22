22c722ff2695bd494394439353025784b9d88806
Iveosk <55953354+Iveosk@users.noreply.github.com>
2020-07-22 23:01:38 +0100

    Created contact us form and page (does not submit to email currently)

--- a2/index.php
+++ a2/index.php
@@ -18,7 +18,7 @@
     	<ul class="flexNav">
   		  <li><a href="index.php">Home</a></li>
   		 	<li><a href="#introduction">Introduction</a></li>
-  	   	<li><a href="/wp/a2/pages/letters.html">Letters</a>
+  	   	<li><a href="">Letters</a>
   	   		<ul class="subMenu">
 		   			<li><a href="#">1914</a></li>
   	    		<li><a href="#">1915</a></li>
@@ -29,7 +29,14 @@
         </li>
 		   	<li><a href="/wp/a2/pages/battles.html">Battles</a></li>
   		  <li><a href="/wp/a2/pages/places.html">Places</a></li>
-        <li><a href="#">Related</a></li>
+        <li><a href="#">Related</a>
+          <ul class="subMenu">
+            <li><a href="https://www.aif.adfa.edu.au/showPerson?pid=11163" target="_blank">Summary of Service</a></li>
+            <li><a href="https://recordsearch.naa.gov.au/SearchNRetrieve/Interface/ViewImage.aspx?B=3009496&S=1" target="_blank">Service Record</a></li>
+            <li><a href="https://www.google.com.au/search?hl=en&site=imghp&tbm=isch&source=hp&biw=1920&bih=982&q=Omrah&oq=Omrah&gs_l=img.12...5422.5422.0.6592.1.1.0.0.0.0.212.212.2-1.1.0.msedr...0...1ac.1.62.img..1.0.0.xuc9Jh0uuzM" target="_blank">9th Battalion Brisbane</a></li>
+            <li><a href="https://www.awm.gov.au/sites/default/files/images/collection/items/ACCNUM_LARGE/RCDIG1067548/RCDIG1067548--337-.JPG" target="_blank">Embarkation Roll</a></li>
+          </ul>
+        </li>
     	</ul>
     </nav>
     <!-- 3 Column site content -->
@@ -86,7 +93,7 @@
           <h3><a href="sitemap">Sitemap</a></h3>
         </div>
       	<div class="links-item">
-          <h3><a href="contact-us">Contact us</a></h3>
+          <h3><a href="/wp/a2/pages/contact.html">Contact us</a></h3>
         </div>
         <div class="links-item">
           <h3><a href="https://github.com/s3683007/wp">GitHub</a></h3>
